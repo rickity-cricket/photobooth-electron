8bfef288c0240d81869d4f3a8a81cad615d05f21
jake <jakemdoran@gmail.com>
2017-04-19 12:59:49 +0100

    countdown added and video captured

---
+++ src/video.js
@@ -0,0 +1,29 @@
+const constraints = {
+  audio: false,
+  video: {
+    mandatory: {
+      minWidth: 100,
+      minHeight: 100,
+      maxWidth: 853,
+      maxHeight: 480
+    }
+  }
+}
+
+function handleSuccess (videoEl, stream) {
+  videoEl.src = window.URL.createObjectURL(stream)
+}
+
+function handleError(error) {
+  console.log('Camera err!', error);
+}
+
+exports.init = (nav, videoEl) => {
+  nav.getUserMedia = nav.webkitGetUserMedia
+  nav.getUserMedia(constraints, stream => handleSuccess(videoEl, stream), handleError)
+}
+
+exports.captureBytes = (videoEl, ctx, canvasEl) => {
+  ctx.drawImage(videoEl, 0, 0)
+  return canvasEl.toDataURL('image/png')
+}
